7b48907f5baa09cb748a3f43c0a69e546db59cf3
EkaterinaShulga <102359023+EkaterinaShulga@users.noreply.github.com>
2023-03-15 12:31:05 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/shelterforanimals/ShelterForAnimalsApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.shelterforanimals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterForAnimalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelterForAnimalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/shelterforanimals/ShelterForAnimalsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.shelterforanimals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterForAnimalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
